de60dcb0d5cd4ab2609d2758f4b44e9b2cccb59a
Zahar <83747187+ZaharrRR@users.noreply.github.com>
2025-11-23 17:00:14 +0500

    Task 1

--- Tyuiu.GalchinskayaVV.Sprint4.Task1.V9.Test/DataServiceTest.cs
+++ Tyuiu.GalchinskayaVV.Sprint4.Task1.V9.Test/DataServiceTest.cs
@@ -1,11 +1,22 @@
-﻿namespace Tyuiu.GalchinskayaVV.Sprint4.Task1.V9.Test
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.GalchinskayaVV.Sprint4.Task1.V9.Lib;
+
+namespace Tyuiu.GalchinskayaVV.Sprint4.Task1.V9.Test
 {
     [TestClass]
-    public sealed class DataServiceTest
+    public class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CheckCalculate()
         {
+            DataService ds = new DataService();
+
+            int[] array = { 6, 3, 3, 7, 7, 7, 7, 3, 3, 6, 4, 4, 8 };
+
+            int actual = ds.Calculate(array);
+            int expected = 4608;
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
--- Tyuiu.GalchinskayaVV.Sprint4.Task1.V9/Program.cs
+++ Tyuiu.GalchinskayaVV.Sprint4.Task1.V9/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Tyuiu.GalchinskayaVV.Sprint4.Task1.V9.Lib;
+
+namespace Tyuiu.GalchinskayaVV.Sprint4.Task1.V9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Гальчинская Виктория Владимировна | ПКТб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы                                                *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #9                                                              *");
+            Console.WriteLine("* Выполнил: Гальчинская Виктория Владимировна | ПКТб-25-1                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан массив из 13 целых чисел [3;8]. Подсчитать произведение чётных      *");
+            Console.WriteLine("* элементов массива.                                                      *");
+            Console.WriteLine("***************************************************************************");
+
+            int n = 13;
+            int[] array = new int[n];
+
+            Console.WriteLine("Введите 13 целых чисел от 3 до 8:");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"array[{i}] = ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            DataService ds = new DataService();
+            int result = ds.Calculate(array);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Произведение чётных элементов массива = " + result);
+
+            Console.ReadKey();
+        }
+    }
+}
