76d78fa7633a26d8a579050bd3d9c22fa261ca13
Zahar <83747187+ZaharrRR@users.noreply.github.com>
2025-11-23 16:58:46 +0500

    Task 0

--- Tyuiu.GalchinskayaVV.Sprint4.Task0.V15.Test/DataServiceTest.cs
+++ Tyuiu.GalchinskayaVV.Sprint4.Task0.V15.Test/DataServiceTest.cs
@@ -1,11 +1,22 @@
-﻿namespace Tyuiu.GalchinskayaVV.Sprint4.Task0.V15.Test
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.GalchinskayaVV.Sprint4.Task0.V15.Lib;
+
+namespace Tyuiu.GalchinskayaVV.Sprint4.Task0.V15.Test
 {
     [TestClass]
-    public sealed class DataServiceTest
+    public class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CheckGetMultOddArrEl()
         {
+            DataService ds = new DataService();
+
+            int[] array = { 9, 8, 7, 6, 5, 7, 3, 2, 7, 3 };
+
+            int actual = ds.GetMultOddArrEl(array);
+            int expected = 138915;
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
--- Tyuiu.GalchinskayaVV.Sprint4.Task0.V15/Program.cs
+++ Tyuiu.GalchinskayaVV.Sprint4.Task0.V15/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Tyuiu.GalchinskayaVV.Sprint4.Task0.V15.Lib;
+
+namespace Tyuiu.GalchinskayaVV.Sprint4.Task0.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Гальчинская Виктория Владимировна | ПКТб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #15                                                             *");
+            Console.WriteLine("* Выполнил: Гальчинская Виктория Владимировна | ПКТб-25-1                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан массив {9, 8, 7, 6, 5, 7, 3, 2, 7, 3}.                              *");
+            Console.WriteLine("* Найти произведение нечётных элементов массива.                          *");
+            Console.WriteLine("***************************************************************************");
+
+            int[] array = { 9, 8, 7, 6, 5, 7, 3, 2, 7, 3 };
+
+            Console.WriteLine("Исходный массив:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            int result = ds.GetMultOddArrEl(array);
+
+            Console.WriteLine("Произведение нечётных элементов массива = " + result);
+
+            Console.ReadKey();
+        }
+    }
+}
