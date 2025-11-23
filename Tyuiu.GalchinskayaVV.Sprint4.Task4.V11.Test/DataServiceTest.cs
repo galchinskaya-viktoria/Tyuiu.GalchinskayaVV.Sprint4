bfd32a17124c389ae2a6e944d5f2f4564ea869cd
Zahar <83747187+ZaharrRR@users.noreply.github.com>
2025-11-23 17:13:25 +0500

    Task 4

--- Tyuiu.GalchinskayaVV.Sprint4.Task4.V11.Test/DataServiceTest.cs
+++ Tyuiu.GalchinskayaVV.Sprint4.Task4.V11.Test/DataServiceTest.cs
@@ -1,11 +1,29 @@
-﻿namespace Tyuiu.GalchinskayaVV.Sprint4.Task4.V11.Test
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.GalchinskayaVV.Sprint4.Task4.V11.Lib;
+
+namespace Tyuiu.GalchinskayaVV.Sprint4.Task4.V11.Test
 {
     [TestClass]
-    public sealed class DataServiceTest
+    public class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CheckCalculate()
         {
+            DataService ds = new DataService();
+
+            int[,] matrix =
+            {
+                { 5, 7, 7, 8, 5 },
+                { 6, 5, 6, 8, 6 },
+                { 7, 6, 8, 8, 5 },
+                { 7, 6, 7, 8, 6 },
+                { 7, 6, 7, 7, 5 }
+            };
+
+            int actual = ds.Calculate(matrix);
+            int expected = 81;
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
--- Tyuiu.GalchinskayaVV.Sprint4.Task4.V11/Program.cs
+++ Tyuiu.GalchinskayaVV.Sprint4.Task4.V11/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Tyuiu.GalchinskayaVV.Sprint4.Task4.V11.Lib;
+
+namespace Tyuiu.GalchinskayaVV.Sprint4.Task4.V11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Гальчинская Виктория Владимировна | ПКТб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы                                                 *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнил: Гальчинская Виктория Владимировна | ПКТб-25-1                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Двумерный массив 5x5 (значения с клавиатуры, от 5 до 8). Найти сумму   *");
+            Console.WriteLine("* нечётных элементов массива.                                            *");
+            Console.WriteLine("***************************************************************************");
+
+            int rows = 5;
+            int cols = 5;
+            int[,] matrix = new int[rows, cols];
+
+            Console.WriteLine("Введите элементы массива 5x5 (целые числа от 5 до 8):");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write($"[{i},{j}] = ");
+                    matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            DataService ds = new DataService();
+            int result = ds.Calculate(matrix);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Сумма нечётных элементов массива = " + result);
+
+            Console.ReadKey();
+        }
+    }
+}
